071da0dfeb01fcbefbe508db7d3b19b8df31445c
叶金耘 <ye_093@163.com>
2021-03-10 16:00:40 +0800

    jenkins demo

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,5 +10,11 @@ pipeline {
                 '''
             }
         }
+
+        stage('Test') {
+            steps {
+                sh './gradlew check'
+            }
+        }
     }
 }
---
+++ src/test/java/cn/yejy/test/AppTest.java
@@ -0,0 +1,12 @@
+package cn.yejy.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testTrue() {
+        Assert.assertTrue(3 == 30 / 10);
+    }
+}
