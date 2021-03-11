847f86dea38408107f6e9a3ac76d6f3c9ac10d67
叶金耘 <ye_093@163.com>
2021-03-11 15:36:55 +0800

    修改为测试环境

--- src/main/java/cn/yejy/config/ApplicationConfig.java
+++ src/main/java/cn/yejy/config/ApplicationConfig.java
@@ -5,6 +5,6 @@ package cn.yejy.config;
  * 系统配置项
  */
 public class ApplicationConfig {
-    public static final String HOST = "yejinyun.cn";
-    public static final String HOST_NAME = "正式环境";
+    public static final String HOST = "localhost";
+    public static final String HOST_NAME = "测试环境";
 }
