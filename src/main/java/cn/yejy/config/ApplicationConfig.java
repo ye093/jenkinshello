e8ea449a9e5818c1b97bd7b7911210fee02ab790
叶金耘 <ye_093@163.com>
2021-03-11 15:34:45 +0800

    增加合并忽略文件

---
+++ src/main/java/cn/yejy/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package cn.yejy.config;
+
+
+/**
+ * 系统配置项
+ */
+public class ApplicationConfig {
+    public static final String HOST = "yejinyun.cn";
+    public static final String HOST_NAME = "正式环境";
+}
--- src/main/java/cn/yejy/hello/App.java
+++ src/main/java/cn/yejy/hello/App.java
@@ -1,5 +1,6 @@
 package cn.yejy.hello;
 
+import cn.yejy.config.ApplicationConfig;
 import cn.yejy.onestore.MemberRepository;
 import cn.yejy.onestore.User;
 import cn.yejy.onestore.UserRepository;
@@ -7,20 +8,7 @@ import cn.yejy.onestore.UserRepository;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("你好啊");
-        System.out.println("1111");
-        System.out.println("2222");
-        System.out.println("4444");
-        System.out.println("555");
-        System.out.println("666");
-
-        System.out.println("User start saving...");
-
-        User user = new User("叶", 18);
-
-        System.out.printf("store result %b%n", UserRepository.save(user));
-
-        MemberRepository memberRepository = new MemberRepository();
-        System.out.println(memberRepository.member("abc13232"));
+        System.out.println(ApplicationConfig.HOST);
+        System.out.println(ApplicationConfig.HOST_NAME);
     }
 }
